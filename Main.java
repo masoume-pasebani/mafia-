599a04675f1593fc91cd8ec744def6bebae86dd7
masoume pasebani <mwsmee@gmail.com>
2021-03-30 12:39:54 +0430

    i added s.th about night

--- Main.java
+++ Main.java
@@ -7,30 +7,35 @@ public class Main {
         String player_names;
         Player p = new Player();
         int num;
-        boolean alive=true;
 
 
         if (sc.next().equals("create_game")) {
             player_names = sc.nextLine();
             String[] arr = player_names.split(" ");
-            num = arr.length;
+            num = arr.length-1;
+            int isalive=num;
             Player[] players = new Player[num];
-            for (int i = 0; i < num - 1; i++) {
+            for (int i = 0; i < num ; i++) {
                 players[i] = new Player();
             }
             String[] a;
-            for (int i = 0; i < num - 1; i++) {
+            int number_of_roles=num;
+            for (int i = 0; i < num ; i++) {
                 String str = sc.nextLine();
                 a = str.split(" ");
                 players[i].name = a[1];
                 players[i].role = a[2];
-                if (!players[i].name.equals(arr[i+1])) {
-                    System.out.println("user not found");
+                while (!players[i].name.equals(arr[i+1])) {
+                    System.out.println("user not found:/.enter the correct name:");
+                    players[i].name=sc.next();
+                }while (number_of_roles!=num){
+                    System.out.println("role not found:/");
                 }
 
             }
+
             if (sc.next().equals("start_game")) {
-                for (int i = 0; i < num - 1; i++) {
+                for (int i = 0; i < num ; i++) {
                     System.out.println(" " + players[i].name + ":" + players[i].role);
 
                 }
@@ -38,13 +43,13 @@ public class Main {
 
                 System.out.println(" ->Ready? Set! Go.<-");
             }
-            else
-                System.out.println("no game created");
-            while (alive) {
-                int number_of_day = 0;
+            else if(sc.next().equals("start_game"))
+                System.out.println("no game created:/");
+            int number_of_day = 0;
+            while (isalive!=0) {
                 number_of_day+=1;
                 System.out.println(" Day" + number_of_day);
-                for (int i = 0; i < num - 1; i++) {
+                for (int i = 0; i < num ; i++) {
                     players[i].vote();
                 }
                 int max = 0;
@@ -52,26 +57,27 @@ public class Main {
                 String max_role = " ";
 
                 if (sc.next().equals("end_vote")) {
-                    for (int i = 0; i < num - 1; i++) {
+                    for (int i = 0; i < num ; i++) {
                         for (int j = i; j < num - 1; j++) {
                             if (players[i].name.equals(players[j].votee_name)) {
                                 players[i].count += 1;
                             }
                         }
                     }
-                    for (int i = 0; i < num - 1; i++) {
+                    for (int i = 0; i < num ; i++) {
                         if (players[i].count > max) {
                             max = players[i].count;
                             max_name = players[i].name;
                             max_role = players[i].role;
                         }
                     }
-                    for (int i = 0; i < num - 1; i++) {
+                    for (int i = 0; i < num ; i++) {
                         if (max > players[i].count && max_role.equals("Joker")) {
                             System.out.println(" Joker won!");
                             System.exit(0);
                         } else if (max > players[i].count) {
                             System.out.println(" " + max_name + " died");
+                            players[i].alive=false;
                             break;
                         } else {
                             System.out.println(" nobody died");
@@ -83,6 +89,11 @@ public class Main {
                 int number_of_night = 0;
                     number_of_night+=1;
                     System.out.println(" Night " + number_of_night);
+                    for (int i=0;i<num;i++){
+                        if(players[i].alive && (players[i] instanceof mafia||players[i] instanceof godfather|| players[i] instanceof silencer||players[i] instanceof doctor||players[i] instanceof detective)){
+                            System.out.println(" "+players[i].name+":"+players[i].role);
+                        }
+                    }
                     for(int i=0;i<4;i++) {
                         p.name = sc.next();
                         if (p instanceof doctor) {
@@ -93,16 +104,15 @@ public class Main {
                         else if(p instanceof mafia || p instanceof godfather || p instanceof silencer){
                             p.vote();
                         }
-                        else if(p instanceof detective){
-
-                        }
                     }
 
+
+                number_of_day++;
             }
 
         }
         else
-            System.out.println("no game created");
+            System.out.println("no game created:/");
     }
 }
 
